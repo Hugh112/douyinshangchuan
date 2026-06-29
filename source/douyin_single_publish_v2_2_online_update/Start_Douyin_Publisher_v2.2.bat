1cc56acefb5dc3e7e7d226cbdf5689b18197fb93
Hugh112 <160467709+Hugh112@users.noreply.github.com>
2026-06-29 16:48:15 +0800

    Delete source/douyin_single_publish_v2_2_online_update directory

--- source/douyin_single_publish_v2_2_online_update/Start_Douyin_Publisher_v2.2.bat
+++
@@ -1,3 +0,0 @@
-﻿@echo off
-cd /d "%~dp0"
-cscript //nologo "Start_Douyin_Publisher_v2.2.vbs"
--- source/douyin_single_publish_v2_2_online_update/安装依赖_可选.bat
+++
@@ -1,9 +0,0 @@
-﻿@echo off
-cd /d "%~dp0"
-set PYTHONUTF8=1
-set PYTHONIOENCODING=utf-8
-py -m pip install --upgrade pip setuptools wheel
-py -m pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
-set PLAYWRIGHT_DOWNLOAD_HOST=https://npmmirror.com/mirrors/playwright
-py -m playwright install chromium
-pause
